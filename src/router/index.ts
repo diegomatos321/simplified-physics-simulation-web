57ad7e0a8a83aef0d488dd78a318b81656c68610
diegomatos321 <diegovasconcelos62@gmail.com>
2025-09-23 11:54:46 -0300

    wip jellycar example

--- src/router/index.ts
+++ src/router/index.ts
@@ -42,12 +42,12 @@ const router = createRouter({
             component: () => import('../views/examples/2d/GjkEpaDemo2.vue'),
         },
         {
-            path: '/demos/2d/gjk_demo_3',
-            name: 'gjk_demo_3',
+            path: '/demos/2d/jelly-car',
+            name: 'jelly-car',
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/examples/2d/GjkEpaDemo3.vue'),
+            component: () => import('../views/examples/2d/JellyCar.vue'),
         },
     ],
 });
---
+++ src/views/examples/2d/JellyCar.vue
@@ -0,0 +1,191 @@
+<style></style>
+
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-3xl"><strong>GJK/EPA Engine</strong></h1>
+        <p>Polygons vs Polygons demo</p>
+
+        <div class="flex">
+            <div class="relative">
+                <div class="absolute">FPS {{ fps }}</div>
+                <div ref="sketchContainer"></div>
+            </div>
+            <div>
+                <div>
+                    <label for="debug">Debug Mode</label>
+                    <input id="debug" name="debug" type="checkbox" v-model="debug" />
+                </div>
+
+                <div>
+                    <label for="pauseOnCollision">Pause on Collision</label>
+                    <input id="pauseOnCollision" name="pauseOnCollision" type="checkbox" v-bind:value="engine.pauseOnCollision" @click="OnPauseCollisionBtn" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+import p5 from 'p5';
+import RectangleBody from '@/physics/RectangleBody';
+import Engine from '@/core/Engine';
+import type Body from '@/physics/Body';
+import PolygonBody from '@/physics/PolygonBody';
+import { vec3 } from 'gl-matrix';
+
+const sketchContainer = ref<HTMLCanvasElement | null>(null);
+let sketchInstance: p5 | null = null;
+let engine = new Engine({
+    top: [-300, -300],
+    right: [300, 300],
+});
+let debug = false;
+let entities: { uvs: [number, number][]; indices: number[]; body: Body }[] = [];
+let player: PolygonBody;
+let texture: p5.Image;
+const fps = ref(0);
+
+onMounted(() => {
+    if (!sketchContainer.value) return;
+
+    const sketch = (p: p5) => {
+        p.setup = () => setup(p);
+        p.draw = () => loop(p);
+    };
+
+    sketchInstance = new p5(sketch);
+    window.addEventListener('keydown', handleKeyDown);
+});
+
+async function setup(p: p5) {
+    if (sketchContainer.value === null) return;
+
+    p.createCanvas(600, 600, p.WEBGL).parent(sketchContainer.value);
+
+    texture = await p.loadImage('/pizza-sprite.png');
+
+    player = PolygonBody.PolygonBuilder(0, 0, 50, 10);
+    engine.bodies.push(player);
+    const { uvs: playerUvs, indices: playerIndices } = player.triangulation();
+    entities.push({
+        uvs: playerUvs,
+        indices: playerIndices,
+        body: player,
+    });
+
+    const ground = new RectangleBody(0, 200, 300, 50);
+    engine.bodies.push(ground);
+    const { uvs: groundUvs, indices: groundIndices } = ground.triangulation();
+    entities.push({
+        uvs: groundUvs,
+        indices: groundIndices,
+        body: ground,
+    });
+}
+
+function loop(p: p5) {
+    p.background(220);
+
+    fps.value = Math.round(p.frameRate());
+
+    engine.step(p.deltaTime / 1000);
+
+    for (const entity of entities) {
+        if (debug) {
+            // Draw constraints
+            p.stroke(0, 0, 0);
+            p.strokeWeight(1);
+            for (const constraint of entity.body.constraints) {
+                p.line(constraint.p0.position[0], constraint.p0.position[1], constraint.p1.position[0], constraint.p1.position[1]);
+            }
+
+            for (const collider of entity.body.colliders) {
+                p.stroke(150, 200, 255);
+                p.strokeWeight(2);
+                const convexHull = entity.body.convexHull();
+                for (let i = 0; i < convexHull.length; i++) {
+                    const v1 = convexHull[i];
+                    const v2 = convexHull[(i + 1) % convexHull.length];
+                    p.line(v1.position[0], v1.position[1], v2.position[0], v2.position[1]);
+                }
+
+                const bodyHull = new PolygonBody([]);
+                bodyHull.particles = convexHull;
+                let edge = bodyHull.getFarthestEdgeInDirection(collider.normal);
+                for (const particle of edge) {
+                    p.noStroke();
+                    p.stroke(255, 0, 0);
+                    p.strokeWeight(5);
+                    p.point(particle.position[0], particle.position[1]);
+
+                    p.stroke(255, 0, 0);
+                    p.strokeWeight(1);
+
+                    const delta = vec3.scale(vec3.create(), vec3.negate(vec3.create(), collider.normal), 20);
+                    const p2 = vec3.add(vec3.create(), particle.position, delta);
+                    p.line(particle.position[0], particle.position[1], p2[0], p2[1]);
+                }
+            }
+        } else {
+            p.texture(texture);
+            p.textureMode(p.NORMAL);
+            p.noStroke();
+            p.beginShape(p.TRIANGLES);
+
+            for (let i = 0; i < entity.indices.length; i++) {
+                const indice = entity.indices[i];
+                const particle = entity.body.particles[indice];
+                const uv = entity.uvs[indice];
+                p.vertex(particle.position[0], particle.position[1], 0, uv[0], uv[1]);
+            }
+
+            p.endShape();
+        }
+    }
+}
+
+onBeforeUnmount(() => {
+    console.log('Clean up');
+    // isRunning = false;
+
+    // if (animationId) {
+    //     cancelAnimationFrame(animationId);
+    // }
+
+    if (sketchInstance) {
+        sketchInstance.remove();
+        sketchInstance = null;
+    }
+
+    window.removeEventListener('keydown', handleKeyDown);
+});
+
+function OnPauseCollisionBtn() {
+    engine.pauseOnCollision = !engine.pauseOnCollision;
+}
+
+function handleKeyDown(e: KeyboardEvent) {
+    if (e.code == 'Space') {
+        engine.isPaused = !engine.isPaused;
+        engine.skip = !engine.skip;
+    }
+
+    if (e.code == 'KeyD') {
+        for (const p of player.particles) {
+            p.move(vec3.fromValues(1, 0, 0));
+        }
+    }
+    if (e.code == 'KeyA') {
+        for (const p of player.particles) {
+            p.move(vec3.fromValues(-1, 0, 0));
+        }
+    }
+
+    if (e.code == 'KeyW') {
+        for (const p of player.particles) {
+            p.move(vec3.fromValues(0, -1, 0));
+        }
+    }
+}
+</script>
